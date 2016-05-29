35982558dd5812326e86507b95e1ee2e8f0b70ec
Garikk <supergarikk@gmail.com>
2016-05-29 20:53:59 +0300

    Make MaryTTS support

--- src/main/java/kkdev/kksystem/plugin/tts/engines/marytts/MaryTTS.java
+++ src/main/java/kkdev/kksystem/plugin/tts/engines/marytts/MaryTTS.java
@@ -5,29 +5,64 @@
  */
 package kkdev.kksystem.plugin.tts.engines.marytts;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import kkdev.kksystem.base.interfaces.IPluginKKConnector;
 import kkdev.kksystem.plugin.tts.engines.ITTSEngine;
 import marytts.LocalMaryInterface;
+import marytts.MaryInterface;
+import marytts.config.MaryConfig;
 import marytts.exceptions.MaryConfigurationException;
 import marytts.exceptions.SynthesisException;
 import marytts.util.MaryRuntimeUtils;
+import static sun.nio.ch.IOUtil.load;
 
 /**
  *
  * @author sayma_000
  */
 public class MaryTTS implements ITTSEngine {
-    LocalMaryInterface mary = null;
+    MaryInterface mary = null;
+    Clip clip;
     boolean State;
-    public MaryTTS()
+    public  MaryTTS()
     {
-        State=true;
+      InitTTS();
+    }
+    private synchronized void InitTTS()
+    {
+      State=true;
         try {
-            MaryRuntimeUtils.getVoices();
+            URLClassLoader CLoader;
+          try {
+              CLoader = new URLClassLoader(new URL[]{(new File("../lib/marytts-common-5.2-beta3.jar")).toURI().toURL()});
+                mary = (LocalMaryInterface) CLoader.loadClass("LocalMaryInterface.class").newInstance();
+          } catch (MalformedURLException ex) {
+              Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
+          } catch (ClassNotFoundException ex) {
+              Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
+          } catch (InstantiationException ex) {
+              Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
+          } catch (IllegalAccessException ex) {
+              Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
+          }
+                //
+              
             mary = new LocalMaryInterface();
+            clip = AudioSystem.getClip();
         } catch (MaryConfigurationException ex) {
+            State=false;
+            Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (LineUnavailableException ex) {
             State=false;
             Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -40,8 +75,18 @@ public class MaryTTS implements ITTSEngine {
            AudioInputStream audio = null;
         try {
             audio = mary.generateAudio(Text);
+            clip.open(audio);
+            clip.start();
+            while (clip.getMicrosecondLength() != clip.getMicrosecondPosition()) {
+            //Let the clip finish playing before closing
+            }           
+
         } catch (SynthesisException ex) {
             Logger.getLogger(MaryTTS.class.getName()).log(Level.ALL, null, ex);
+        } catch (LineUnavailableException ex) {
+            Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MaryTTS.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
