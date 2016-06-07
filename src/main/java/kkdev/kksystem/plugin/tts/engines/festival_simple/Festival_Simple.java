20bcba31bb91659ab6f86d2986b0b3d7d4cd92f1
Garikk <supergarikk@gmail.com>
2016-06-07 17:05:52 +0300

    simple tts completed!!

--- src/main/java/kkdev/kksystem/plugin/tts/engines/festival_simple/Festival_Simple.java
+++ src/main/java/kkdev/kksystem/plugin/tts/engines/festival_simple/Festival_Simple.java
@@ -5,36 +5,57 @@
  */
 package kkdev.kksystem.plugin.tts.engines.festival_simple;
 
-
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import kkdev.kksystem.plugin.tts.engines.ITTSEngine;
 
 /**
  *
  * @author sayma_000
  */
-public class Festival_Simple implements ITTSEngine {
- 
+public class Festival_Simple {
 
+    Queue<String> SayQueue;
+    boolean Start=false;
     boolean State;
-
-    @Override
-    public void SayText(String Text) {
-        try {
-             String s = null;
-            //TODO WARNING!!!! CHANGE THIS!!!
-           
-         //Runtime.getRuntime().exec("./tts.sh \"" + Text+"\"");
-         Runtime.getRuntime().exec("./tts.sh " + Text);
-         
-            //==============
-        } catch (IOException ex) {
-            Logger.getLogger(Festival_Simple.class.getName()).log(Level.SEVERE, null, ex);
+    Thread SayWork = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Festival_Simple.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if (SayQueue.size() > 0) {
+                    try {
+                        String Text = SayQueue.poll();
+                        Process P;
+                        P = Runtime.getRuntime().exec("./tts.sh " + Text);
+                        P.waitFor();
+                    } catch (IOException ex) {
+                        Logger.getLogger(Festival_Simple.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Festival_Simple.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
         }
-    }
-      
-        
+    });
+
+    public Festival_Simple() {
+        SayQueue = new ArrayDeque<>();
     }
 
+    public void SayText(String Text) {
+        SayQueue.add(Text);
+        if (!Start)
+        {
+            SayWork.start();
+            Start=true;
+        }
+
+    }
+}
--- src/main/java/kkdev/kksystem/plugin/tts/manager/TTSManager.java
+++ src/main/java/kkdev/kksystem/plugin/tts/manager/TTSManager.java
@@ -1,22 +1,16 @@
 package kkdev.kksystem.plugin.tts.manager;
 
-import kkdev.kksystem.base.classes.base.PinBaseDataTaggedObj;
 import kkdev.kksystem.base.classes.notify.NotifyConsts.NOTIFY_METHOD;
 import kkdev.kksystem.base.classes.notify.PinNotifyData;
 import kkdev.kksystem.base.classes.plugins.PluginMessage;
 import kkdev.kksystem.base.classes.plugins.simple.managers.PluginManagerBase;
 import kkdev.kksystem.base.constants.PluginConsts;
-import static kkdev.kksystem.base.constants.PluginConsts.KK_PLUGIN_BASE_BASIC_TAGGEDOBJ_DATA;
-import kkdev.kksystem.base.constants.SystemConsts;
 import kkdev.kksystem.plugin.tts.KKPlugin;
-import kkdev.kksystem.plugin.tts.configuration.PluginSettings;
-import kkdev.kksystem.plugin.tts.engines.ITTSEngine;
-import kkdev.kksystem.plugin.tts.engines.externalttsserver.ExternalTTS;
 import kkdev.kksystem.plugin.tts.engines.festival_simple.Festival_Simple;
 
 public class TTSManager extends PluginManagerBase {
     
-    ITTSEngine Engine;
+    Festival_Simple Engine;
 
     public void Init(KKPlugin Conn) {
         this.Connector = Conn;
